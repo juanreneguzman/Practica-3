e56d81027ec06e4394dd86df14e585f5e80b123e
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:11:35 -0500

    2

---
+++ EJERCICIO 2/EJERCICIO 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace el_mayor_es
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un entero de dos cifras");
+            int entero = int.Parse(Console.ReadLine());
+
+            int n1 = entero / 10;
+            int n2 = entero %10;
+            
+            if (n1 > n2)
+            {
+
+                Console.WriteLine($"{n1} es mayor que {n2}");
+
+            }
+            else { Console.WriteLine($"{n2} es mayor que {n1}"); }
+
+        }
+    }
+}

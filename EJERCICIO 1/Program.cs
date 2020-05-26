975a2ef13ddc61e9e0ef9814cb5d17c3be0fde6a
juanreneguzman <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 06:08:14 -0500

    initial commit

---
+++ EJERCICIO 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un numero entero");
+
+            int numero = Int32.Parse(Console.ReadLine());
+
+
+
+             numero = numero %= 2;
+
+            if (numero == 0) 
+            {
+                Console.WriteLine("El numero es par");
+
+            }
+            else { Console.WriteLine("El numero es impar"); }
+
+
+
+
+        }
+
+        
+    }
+}

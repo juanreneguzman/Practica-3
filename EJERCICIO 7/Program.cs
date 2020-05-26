c5c6bf0a6ed7a94b21ed25b1ff756a3c73b4fdc4
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:21:45 -0500

    Add files via upload

---
+++ EJERCICIO 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UN NUMERO");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 4 == 0) {
+
+                Console.WriteLine("el numero es multiplo de 4");
+
+            }
+            if (n1 % 5 == 0)
+            {
+                Console.WriteLine("El numero es multiplo de 5");
+            }
+        }
+    }
+}

4b134dcd44e5733b92714032dd0a330451c8687c
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:20:05 -0500

    Add files via upload

---
+++ EJERCICIO 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tarea_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un numero");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite otro numero");
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 != 0)
+            {
+
+                Console.WriteLine($"El resultado es {n1/n2}");
+
+            }
+            else { Console.WriteLine("Error: no se puede dividir entre 0"); }
+        }
+
+    }
+}

2cc77345dd49610a277672a7e343492b6227c8cb
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:15:32 -0500

    4

---
+++ EJERCICIO 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UN NUMERO ENTERO");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 10 == 0)
+            {
+                Console.WriteLine("El numero es multiplo de 10. Digite otro numero");
+                int n2 = int.Parse(Console.ReadLine());
+
+                if (n2 % 10 == 0)
+                {
+                    Console.WriteLine($"El numero {n2} tambien es multiplo de 10");
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine($"EL numero {n1} no es multiplo de 10");
+            }
+        }  
+
+    }
+}

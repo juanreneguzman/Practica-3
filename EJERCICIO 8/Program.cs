0fdc12ebdf5fabb3c2d6748fc3b04e6342906606
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:36:37 -0500

    Add files via upload

---
+++ EJERCICIO 8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EJERCICIO_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UN NUMERO ENTERO");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1>0 && n2>0)
+            {
+                Console.WriteLine("LOS DOS NUMEROS SON POSITIVOS");
+
+            }
+            if (n1 < 0 && n2 < 0)
+            {
+                Console.WriteLine("LOS DOS NUMEROS SON NEGATIVOS");
+
+            }
+
+            if (n1>0 || n2 > 0)
+            {
+                Console.WriteLine("Uno de los numeros es positivo");
+            }
+
+        }
+    }
+}

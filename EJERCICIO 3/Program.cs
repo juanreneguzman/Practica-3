e404eb49bbcbb63383d2a8fa29dfdc89746d8f47
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:13:34 -0500

    3

---
+++ EJERCICIO 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace EJERCICIOS_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UN NUMERO");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE OTRO NUMERO");
+            int n2 = int.Parse(Console.ReadLine());
+
+          
+
+            if (n1%n2==0)
+
+            {
+                Console.WriteLine($"El numero {n1} es multiplo de {n2}");
+
+
+                }
+
+            else
+            {
+                Console.WriteLine("El numero no es multiplo");
+            }
+        }
+    }
+}

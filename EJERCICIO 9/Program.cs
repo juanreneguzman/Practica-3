74657bd24a2b0bbacf86282455fc3379aa4db38c
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 17:35:25 -0500

    Add files via upload

---
+++ EJERCICIO 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE 3 NUMEROS");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            
+            if (n1>n2 || n1 > n3 )
+            {
+                Console.WriteLine($"El numero {n1} es mayor que {n2} y {n3}");
+            }
+
+            if (n2 > n1 || n2 > n3)
+            {
+                Console.WriteLine($"El numero {n2} es mayor que {n1} y {n3}");
+            }
+
+            if (n3 > n1 || n3 > n2)
+            {
+                Console.WriteLine($"El numero {n3} es mayor que {n1} y {n2}");
+            }
+        }
+    }
+}

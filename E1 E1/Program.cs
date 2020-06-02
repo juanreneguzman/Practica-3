d25b7fe3fdce8ff910ce100577ef42c336410ab3
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-06-02 18:38:35 -0500

    ejercicio 1 del primer parcial

---
+++ E1 E1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un dia de la semana");
+            string dia = Console.ReadLine();
+
+
+
+            if (dia == "sabado" || dia == "domingo") {
+
+                Console.WriteLine("Es fin de semana");
+
+               
+            }
+            if (dia == "lunes" || dia == "martes" || dia == "miercoles" || dia == "jueves" || dia == "viernes")
+            {
+                Console.WriteLine("Es dia de semana");
+            }
+            if (dia != "lunes" && dia != "martes" && dia != "miercoles" && dia != "jueves" && dia != "viernes" && dia != "sabado" && dia != "domingo")
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+
+
+        }
+    }
+}

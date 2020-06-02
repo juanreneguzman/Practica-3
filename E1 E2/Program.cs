1ec206abaf2a249232acc9d4ffc124954e36df53
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-06-02 18:42:34 -0500

    ejercicio 2 del primer parcial
    ejercicio 2 del primer parcial

---
+++ E1 E2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es el precio del producto");
+            int price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1- para efectivo 2- para tarjeta");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a== 1) {
+                Console.WriteLine("Gracias por su compra");
+
+            
+            }
+            else
+            {
+                Console.WriteLine("Digite el numero de 12 digitos de su targeta");
+                long b = long.Parse(Console.ReadLine());
+                
+                Console.WriteLine("gracias por su compra");
+            }
+
+        }
+    }
+}
